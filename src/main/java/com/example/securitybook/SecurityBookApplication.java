d37c17d2fcdb5979b2c2a0eb02bc41ad8e6a6b85
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-10 10:15:53 +0900

    ch2-ex1

---
+++ src/main/java/com/example/securitybook/SecurityBookApplication.java
@@ -0,0 +1,13 @@
+package com.example.securitybook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityBookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityBookApplication.class, args);
+    }
+
+}
