07929bff294671182f7cc76f782937433fbe19d0
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-14 12:23:29 +0900

    ch3-ex2 : UserDetailsManager로 JdbccUserDetailsManager 사용

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -1,24 +1,26 @@
 package com.example.securitybook.config;
 
-import com.example.securitybook.model.User;
-import com.example.securitybook.services.InMemoryUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
+
 
 @Configuration
 public class ProjectConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        UserDetails u = new User("john", "12345", "read");
-        List<UserDetails> users = List.of(u);
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        String usersByUsernameQuery = "select username, password, enabled from security_book.users where username = ?";
+        String authsByUserQuery = "select username, authority from security_book.authorities where username = ?";
+        var userDetailsManager = new JdbcUserDetailsManager(dataSource);
+        userDetailsManager.setUsersByUsernameQuery(usersByUsernameQuery);
+        userDetailsManager.setAuthoritiesByUsernameQuery(authsByUserQuery);
+        return userDetailsManager;
     }
 
     @Bean
