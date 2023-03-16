cf37722a206171191703a33361b7e5fc5803574b
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-16 14:33:55 +0900

    ch5-ex4 : formLogin 구현

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -1,7 +1,10 @@
 package com.example.securitybook.config;
 
 
+import com.example.securitybook.handler.CustomAuthenticationFailureHandler;
+import com.example.securitybook.handler.CustomAuthenticationSuccessHandler;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -14,12 +17,20 @@ import org.springframework.security.core.context.SecurityContextHolder;
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    @Autowired
+    private CustomAuthenticationFailureHandler authenticationFailureHandler;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.httpBasic(c -> {
-            c.realmName("OTHER");
-            c.authenticationEntryPoint(new CustomEntryPoint());
-        });
+        http.formLogin()
+                .successHandler(authenticationSuccessHandler)
+                .failureHandler(authenticationFailureHandler)
+        .and()
+                .httpBasic();
+
 
         http.authorizeRequests().anyRequest().authenticated();
     }
--- src/main/java/com/example/securitybook/controller/HelloController.java
+++ src/main/java/com/example/securitybook/controller/HelloController.java
@@ -6,6 +6,7 @@ import org.springframework.security.concurrent.DelegatingSecurityContextExecutor
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,12 +14,12 @@ import java.util.concurrent.Callable;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-@RestController
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String hello(){
-        return "hello";
+    @GetMapping("/home")
+    public String home(){
+        return "home.html";
     }
 
 
