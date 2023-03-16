724825eff6deb7932d7a31b82652f13dfce5a3ec
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-16 10:46:41 +0900

    ch5-ex2 : SecurityContext 관리 - DelegatingSecurityContextRunnable

--- src/main/java/com/example/securitybook/controller/HelloController.java
+++ src/main/java/com/example/securitybook/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.example.securitybook.controller;
 
 import org.springframework.scheduling.annotation.Async;
+import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RestController
 public class HelloController {
@@ -21,5 +26,20 @@ public class HelloController {
         SecurityContext context = SecurityContextHolder.getContext();
         String username = context.getAuthentication().getName();
         return "bye" + username;
+    }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception{
+        Callable<String> task = () -> {
+            SecurityContext context = SecurityContextHolder.getContext();
+            return context.getAuthentication().getName();
+        };
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            var contextTask = new DelegatingSecurityContextCallable<>(task);
+            return "Ciao, " + e.submit(contextTask).get() + "!";
+        } finally {
+            e.shutdown();
+        }
     }
 }
