289fa1e3c3055e009ce2f3a33bcb1fbebf1ad962
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-16 10:27:37 +0900

    ch5-ex2 : SecurityContext 관리 - MODE_INHERITABLETHREADLOCAL

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -1,21 +1,20 @@
 package com.example.securitybook.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import javax.sql.DataSource;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 
 @Configuration
-public class ProjectConfig extends WebSecurityConfigurerAdapter {
-
+@EnableAsync
+public class ProjectConfig{
+    @Bean
+    public InitializingBean initializingBean() {
+        return () -> SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
 
 }
--- src/main/java/com/example/securitybook/controller/HelloController.java
+++ src/main/java/com/example/securitybook/controller/HelloController.java
@@ -1,6 +1,9 @@
 package com.example.securitybook.controller;
 
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +13,13 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello(Authentication a){
         return "hello" + a.getName() + "!";
+    }
+
+    @GetMapping("/bye")
+    @Async
+    public String goodBye(){
+        SecurityContext context = SecurityContextHolder.getContext();
+        String username = context.getAuthentication().getName();
+        return "bye" + username;
     }
 }
