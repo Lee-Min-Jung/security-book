7cf7aaddcd05fc1ab8876bc88c8e382c439161c8
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-24 10:51:28 +0900

    ch9-ex2 : 특정 위치에 필터 추가

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -1,29 +1,24 @@
 package com.example.securitybook.config;
 
-import com.example.securitybook.filter.AuthenticationLoggingFilter;
-import com.example.securitybook.filter.RequestValidationFilter;
-import org.springframework.context.annotation.Bean;
+
+import com.example.securitybook.filter.StaticKeyAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private StaticKeyAuthenticationFilter filter;
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+                .addFilterAt(filter, BasicAuthenticationFilter.class)
                 .authorizeRequests().anyRequest().permitAll();
     }
 }
