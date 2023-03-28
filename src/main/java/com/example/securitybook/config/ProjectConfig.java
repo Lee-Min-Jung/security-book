c1af3dc68d72666b62c0defe8912267761da7e10
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-28 12:24:19 +0900

    ch10-ex4 : cors 적용 안 된 코드

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -2,31 +2,22 @@ package com.example.securitybook.config;
 
 
 
-import com.example.securitybook.csrf.CustomCsrfTokenRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public CsrfTokenRepository customTokenRepository(){
-        return new CustomCsrfTokenRepository();
-    }
+
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
 
-        http.csrf(c -> {
-            c.csrfTokenRepository(customTokenRepository());
-            c.ignoringAntMatchers("/ciao");
-        });
+        http.csrf().disable();
 
         http.authorizeRequests().anyRequest().permitAll();
     }
--- src/main/java/com/example/securitybook/controller/HelloController.java
+++ src/main/java/com/example/securitybook/controller/HelloController.java
@@ -1,24 +1,27 @@
 package com.example.securitybook.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+import java.util.logging.Logger;
+
+@Controller
 public class HelloController {
 
-    @GetMapping("/hello")
-    public String getHello() {
-        return "Get Hello!";
+    private Logger logger = Logger.getLogger(HelloController.class.getName());
+
+    @GetMapping("/")
+    public String main(){
+        return "main.html";
     }
 
-    @PostMapping("/hello")
-    public String postHello() {
-        return "Post Hello!";
+    @PostMapping("/test")
+    @ResponseBody
+    public String test(){
+        logger.info("Test method called");
+        return "HELLO";
     }
 
-    @PostMapping("/ciao")
-    public String postCiao() {
-        return "Post Ciao";
-    }
 }
