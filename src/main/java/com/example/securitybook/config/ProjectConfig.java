b10e309c885e1a69f881be2dd108d0305fcf0f65
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-28 12:34:12 +0900

    ch10-ex4 : cors 적용 - CorsConfigurer 사용

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -5,7 +5,10 @@ package com.example.securitybook.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.List;
 
 
 @Configuration
@@ -16,7 +19,15 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.cors(c -> {
+            CorsConfigurationSource source = request -> {
+                CorsConfiguration config = new CorsConfiguration();
+                config.setAllowedOrigins(List.of("*"));
+                config.setAllowedMethods(List.of("*"));
+                return config;
+            };
+            c.configurationSource(source);
+        });
         http.csrf().disable();
 
         http.authorizeRequests().anyRequest().permitAll();
