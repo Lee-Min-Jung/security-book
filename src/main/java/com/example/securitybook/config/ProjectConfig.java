6430de08393a266373f3d4cd6b2083ed291491ca
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-29 14:03:05 +0900

    ch11-ex1 : 실전 예제 2 - 인증 서버 구현

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -2,34 +2,27 @@ package com.example.securitybook.config;
 
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import java.util.List;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors(c -> {
-            CorsConfigurationSource source = request -> {
-                CorsConfiguration config = new CorsConfiguration();
-                config.setAllowedOrigins(List.of("*"));
-                config.setAllowedMethods(List.of("*"));
-                return config;
-            };
-            c.configurationSource(source);
-        });
         http.csrf().disable();
-
         http.authorizeRequests().anyRequest().permitAll();
     }
 }
