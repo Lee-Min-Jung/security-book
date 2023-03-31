2a79df4c10fca9c92462f13497ce538e58eb96ea
Lee-Min-Jung <light2009@sookmyung.ac.kr>
2023-03-31 10:21:59 +0900

    ch12-ex1 : OAuth2 적용

--- src/main/java/com/example/securitybook/config/ProjectConfig.java
+++ src/main/java/com/example/securitybook/config/ProjectConfig.java
@@ -6,23 +6,35 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return new BCryptPasswordEncoder();
+    public ClientRegistrationRepository clientRepository() {
+        var c = clientRegistration();
+        return new InMemoryClientRegistrationRepository(c);
     }
 
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId("")
+                .clientSecret("")
+                .build();
+    }
 
     @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests().anyRequest().permitAll();
+    protected void configure(HttpSecurity http) throws Exception{
+        http.oauth2Login(); // OAuth2LoginAuthenticationFilter를 필터 체인에 추가한다. 이 필터는 요청을 가로채고 OAuth2 인증 논리를 적용한다
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
     }
 }
